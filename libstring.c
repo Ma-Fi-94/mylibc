0c5aad616f32ceb344d472fa30520bb371c5e56a
Ma-Fi-94 <33027321+Ma-Fi-94@users.noreply.github.com>
2022-03-08 22:22:17 +0100

    Add files via upload

--- libstring.c
+++ libstring.c
@@ -86,16 +86,96 @@ char *strrchr(const char *s, int c) {
 }
 
 
+// Return number of chars at beginning of s which are in c
+size_t strspn(const char *s, const char *c) {
+    // Lookup table for all chars of c
+    char lu[256] = {0};
+    for (; *c; lu[(int) *c] = 1, c++) {;}
+
+    // Iterate through s as long as chars are element of c and count
+    size_t ctr = 0;
+    for (; *s && lu[(int) *s]; ctr++, s++) {;}
+    return ctr;
+}
+
+
+// Return number of chars at beginning of s which are all not in c
+size_t strcspn(const char *s, const char *c) {
+    // Lookup table for all chars of c
+    char lu[256] = {0};
+    for (; *c; lu[(int) *c] = 1, c++) {;}
+
+    // Iterate through s as long as chars are not element of c and count
+    size_t ctr = 0;
+    for (; *s && !lu[(int) *s]; ctr++, s++) {;}
+    return ctr;
+}
+
+
+// Return pointer to first char in s that is not part of c
+char *strpbrk(const char *s, const char *c) {
+    // Lookup table for all chars of c
+    char lu[256] = {0};
+    for (; *c; lu[(int) *c] = 1, c++) {;}
+
+    // Iterate through s as long as chars are not element of c
+    for (; *s && !lu[(int) *s]; s++) {;}
+    return s;
+}
+
+
+// TODO Return pointer to first occurence of t in s
+// char *strstr(const char *s, const char *t)
+
+
+// TODO Return pointer to string describing error number n
+// char *strerror(int n) {}
+
+
+// TODO Tokenise string s with all chars from c as delimiters
+// char *strtok(char *s, const char* c)
+
+
+// Copy n symbols from t to s, return s
+void *memcpy(void *s, const void *t, size_t n) {
+    char *u = (char*) s;
+    char *v = (char*) t;
+    for (; n--; *u++ = *v++) {;}
+    return (void*) u;
+}
+
+
+// Same as memcpy, but also works if s,t overlap
+void *memmove(void *s, const void *t, size_t n) {
+    if (s > t) {
+        // Source starts after destination - forward copy
+        memcpy(s, t, n);
+    } else {
+        // Else backwards copy
+        char *u = (char*) s;
+        char *v = (char*) t;
+        for (int i = (int) n; i > 0; i--) {
+            u[i-1] = v[i-1];
+        }
+    }
+    return s;
+}
+
+// TODO
+// int memcmp(cs,ct,n)
+// void *memchr(cs,c,n)
+// void *memset(s,c,n)
+
 // Some testing
 int main() {
-    char str1[] = "This is a test";
-    printf("%li\n", strchr(str1, 'a') - str1);
-    printf("%li\n", strchr(str1, 'i') - str1);
-    printf("%p\n", strchr(str1, 'z'));
+    char str1[] = "abcxxxxxxxxx";
+    char str2[] = "123456789abcdef";
+    printf("%i\n", (int) strspn(str1, str2));
 
-    printf("%li\n", strrchr(str1, 'a') - str1);
-    printf("%li\n", strrchr(str1, 'i') - str1);
-    printf("%p\n", strrchr(str1, 'z'));
+    char str3[] = "ghijkl1234";
+    char str4[] = "123456789abcdef";
+    printf("%i\n", (int) strcspn(str3, str4));
+    printf("%c\n", *strpbrk(str3, str4));
 
 
     return 0;
